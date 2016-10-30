8495a7d088ecefd3d65aec2a9cd0836abcdef6bc
paultbarrett <pbarrett@bitsystems.com.au>
2016-10-30 17:09:38 +1100

    Update and rename php7-fpm.sh to php5-fpm.sh

--- php5-fpm.sh
+++ php5-fpm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
-exec php7-fpm -F >>/var/log/php7-fpm/screen_output.log 2>&1
+exec php-fpm5.6 -F >>/var/log/php7-fpm/screen_output.log 2>&1
 
